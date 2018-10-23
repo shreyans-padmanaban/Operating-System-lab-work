9517c237c1887ad8f6b47dfba983f7f63f1dea66
Abhinav Srivastava <36011576+AbhinavMir@users.noreply.github.com>
2018-10-23 19:16:14 +0530

    incomplete

---
+++ disk_scheduling/sstf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+	//this algorithm seeks the closest possible task to the present location of the header
+	int queue[100].t[100],seek=0,n,i,j,temp;
+	float avg;
+	printf("Enter size of queue");
+	scanf("%d",&n);
+	printf("Enter queue");
+	for(i=0;i<n;i++){scanf("%d",&queue[i]);}
+
+}
